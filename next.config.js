ac9f1acbae9400934505c66784be060d536c70fb
Cheyyu07 <Cheyyus786@gmail.com>
2026-05-05 13:06:47 +0100

    feat(pwa): precache illustrations folder in service worker

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const withPWA = require('@ducanh2912/next-pwa').default({
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
+  publicExcludes: ['!illustrations/**/*'],
 })
 
 const nextConfig = {
